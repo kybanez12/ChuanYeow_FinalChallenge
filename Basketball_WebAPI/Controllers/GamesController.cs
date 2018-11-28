6a28081ee8ce43adef65bb5450405bdce557ac95
Kristian <kybanez12@gmail.com>
2018-11-28 16:34:23 +1100

    Initial commit

---
+++ Basketball_WebAPI/Controllers/GamesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Basketball_WebAPI.Models;
+
+namespace Basketball_WebAPI.Controllers
+{
+    [Authorize]
+    public class GamesController : Controller
+    {
+        private BBEntities db = new BBEntities();
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            var games = db.Games.Where(g => g.GameDateTime > DateTime.Now).ToList();
+            return View(games.ToList());
+        }
+
+        // GET: Past Games
+        public ActionResult PastGames()
+        {
+            var games = db.Games.Where(g => g.GameDateTime < DateTime.Now).ToList();
+            return View(games.ToList());
+        }
+
+        // GET: Team Members of Team A
+        public ActionResult TeamA()
+        {
+            var teams = db.Games.Where(m => m.Member.Team == "Team A").ToList();
+            return View(teams.ToList());
+        }
+
+        // GET: Team Members of Team B
+        public ActionResult TeamB()
+        {
+            var teams = db.Games.Where(m => m.Member.Team == "Team B").ToList();
+            return View(teams.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            ViewBag.Email = new SelectList(db.Members, "Email", "Team");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GameID,Outcome,GameDateTime,Venue,Fee,Email")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Email = new SelectList(db.Members, "Email", "Team", game.Email);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Email = new SelectList(db.Members, "Email", "Team", game.Email);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GameID,Outcome,GameDateTime,Venue,Fee,Email")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Email = new SelectList(db.Members, "Email", "Team", game.Email);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Games.Find(id);
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
